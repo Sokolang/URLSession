1c084cb0dd79230e32734901fe1bb38df88a97d1
Anzhelika Sokolova <skelvin.pit@icloud.com>
2022-04-08 13:15:15 +0200

    first commit

---
+++ URLSession/URLSessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLSessionApp.swift
+//  URLSession
+//
+//  Created by Anzhelika Sokolova on 08.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct URLSessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
